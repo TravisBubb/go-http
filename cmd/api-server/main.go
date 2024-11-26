119d443ff61184706847b181e59f5c49061dcfd8
Travis Bubb <travisbubb14@gmail.com>
2024-11-25 21:49:48 -0500

    Add foundation for creating a TCP server and handling incoming connections

---
+++ cmd/api-server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/TravisBubb/go-http/http"
+)
+
+func main() {
+	server := http.CreateServer("localhost", 8080)
+	err := server.Start()
+	if err != nil {
+		log.Fatal("An unexpected error occurred attempting to start server:", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module http
+module github.com/TravisBubb/go-http 
 
 go 1.22.2
